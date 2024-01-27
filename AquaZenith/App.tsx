4242abc04f2c08293f05f714da942d78c15bb482
gageludlow <gageludlow@gmail.com>
2024-01-27 13:24:19 -0800

    fix react native deafult ios simulator that was set to iphone SE, now is iphone 15 pro max. made some test changes to app to see the effects in the simulator.

--- AquaZenith/App.tsx
+++ AquaZenith/App.tsx
@@ -89,7 +89,6 @@ function App(): React.JSX.Element {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
